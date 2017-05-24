c03616e199dbe1b0eb02cf2ba7dfab7386f48bbd
Rodrigo Brayner <rbl2@cin.ufpe.br>
2017-05-24 19:10:43 -0300

    Jogo implementado
    Jogo da velha online implementado. Resta a detecção de perda de conexão e se possível, a interface gráfica.

--- Q3/Player.py
+++ Q3/Player.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import tkinter as tk
 
 
 class Game:
@@ -18,36 +19,69 @@ class Game:
         self.__table = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]
         self.__myMove = (-1, -1)
 
-    def __gameFinished(self):  # A função ainda não retorna que jogador é o vencedor, apenas se o jogo acabou
-        if (self.__table[0][0] == self.__table[0][1] == self.__table[0][2] != -1) or (
-                            self.__table[1][0] == self.__table[1][1] == self.__table[1][2] != -1) or (
-                            self.__table[2][0] == self.__table[2][1] == self.__table[2][2] != -1):
-            # verifica se alguma coluna foi preenchida por apenas x ou apenas o
-            return True
-        elif (self.__table[0][0] == self.__table[1][0] == self.__table[2][0] != -1) or (
-                            self.__table[0][1] == self.__table[1][1] == self.__table[2][1] != -1) or (
-                            self.__table[0][2] == self.__table[1][2] == self.__table[2][2] != -1):
-            # verifica se alguma linha foi preenchida por apenas x ou apenas o
-            return True
-        elif (self.__table[0][0] == self.__table[1][1] == self.__table[2][2] != -1) or (
-                            self.__table[0][2] == self.__table[1][1] == self.__table[2][0] != -1):
-            # verifica as diagonais
-            return True
+    def __gameFinished(self):
+        if (self.__table[0][0] == self.__table[0][1] == self.__table[0][2] != -1):
+            if self.__table[0][0] == choice:
+                return 1
+            else:
+                return 2
+        elif (self.__table[1][0] == self.__table[1][1] == self.__table[1][2] != -1):
+            if self.__table[1][0] == choice:
+                return 1
+            else:
+                return 2
+        elif (self.__table[2][0] == self.__table[2][1] == self.__table[2][2] != -1):
+            if self.__table[2][0] == choice:
+                return 1
+            else:
+                return 2
+        elif (self.__table[0][0] == self.__table[1][0] == self.__table[2][0] != -1):
+            if self.__table[0][0] == choice:
+                return 1
+            else:
+                return 2
+        elif (self.__table[0][1] == self.__table[1][1] == self.__table[2][1] != -1):
+            if self.__table[0][1] == choice:
+                return 1
+            else:
+                return 2
+        elif (self.__table[0][2] == self.__table[1][2] == self.__table[2][2] != -1):
+            if self.__table[0][2] == choice:
+                return 1
+            else:
+                return 2
+        elif (self.__table[0][0] == self.__table[1][1] == self.__table[2][2] != -1):
+            if self.__table[0][0] == choice:
+                return 1
+            else:
+                return 2
+        elif (self.__table[0][2] == self.__table[1][1] == self.__table[2][0] != -1):
+            if self.__table[0][2] == choice:
+                return 1
+            else:
+                return 2
         else:
-            return False
+            for j in self.__table:
+                if (-1) in j:
+                    return 0
+            return 3
 
-    def __myTurn(self) -> None: # Recebe a jogada do usuário e envia ao oponente. (ainda simples, não checa se
-        # posição já está sendo usada)
-        print("Digite as coordenadas (1<=x<=3 e 1<=y<=3)")
-        posX, posY = input().split(" ")
-        self.__myMove = (int(posX) - 1, int(posY) - 1)
-        if (0 <= self.__myMove[0] <= 2) and (0 <= self.__myMove[1] <= 2):
-            self.__me.sendto((str(self.__myMove[0]) + "," + str(self.__myMove[1]) + "," + str(self.__turn)).encode(), self.__Opon)
-            self.__turn += 1
+    def __myTurn(self) -> None:  # Recebe a jogada do usuário e envia ao oponente.
+        while 1:
+            print("Digite as coordenadas 'x y' (1<=x<=3 e 1<=y<=3)")
+            posX, posY = input().split(" ")
+            self.__myMove = (int(posX) - 1, int(posY) - 1)
+            if (0 <= self.__myMove[0] <= 2) and (0 <= self.__myMove[1] <= 2) and (
+                self.__table[self.__myMove[0]][self.__myMove[1]] == -1):
 
-        else:
-            print("Erro, variáveis erradas")
-        return
+                self.__me.sendto(
+                    (str(self.__myMove[0]) + "," + str(self.__myMove[1]) + "," + str(self.__turn)).encode(),
+                    self.__Opon)
+                self.__turn += 1
+                self.__table[self.__myMove[0]][self.__myMove[1]] = choice
+                return
+            else:
+                print("Erro, variáveis erradas")
 
     def __opTurn(self) -> None:
         iniT = time.perf_counter()
@@ -64,14 +98,14 @@ class Game:
                 # Se não for o correto, o pacote é duplicado, então é ignorado.
                 self.__turn += 1
                 self.__table[posX][posY] = (choice + 1) % 2
-                print("Adversario colocou em: ", posX, " ", posY)
-                break
-            if (self.__myMove != (-1, -1)) and (iniT - past >= 1):
+                print("Adversario colocou em: ", posX + 1, " ", posY + 1)
+                return
+            if (self.__myMove != (-1, -1)) and (past - iniT >= 1):
                 # Se tiver se passado 1 segundo, reenvia a jogada (assume que o adversário não recebeu sua jogada)
                 print("reenviando")
-                self.__me.sendto((str(self.__myMove[0]) + "," + str(self.__myMove[1]) + "," + str(self.__turn - 1)).encode(),
-                                 self.__Opon)
-        return
+                self.__me.sendto(
+                    (str(self.__myMove[0]) + "," + str(self.__myMove[1]) + "," + str(self.__turn - 1)).encode(),
+                    self.__Opon)
 
     def run(self):
         while not (self.__gameFinished()):
@@ -85,6 +119,14 @@ class Game:
                     self.__myTurn()
                 else:
                     self.__opTurn()
+        else:
+            x = self.__gameFinished()
+            if x == 1:
+                print("Voce Venceu")
+            elif x == 2:
+                print("Voce eh um bosta")
+            else:
+                print("Deu velha")
         return
 
 
@@ -108,3 +150,4 @@ while 1:
         jogo = Game(s, player, choice)
         print(msg.decode())
         jogo.run()
+        quit()
