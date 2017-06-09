7c5f0dce0c297a861619acc46607404f93cafd0a
Lucas Costa Cabral <lcc4@cin.ufpe.br>
2017-06-09 17:05:40 -0300

    ADD server file
    	Test.py é o arquivo de testes, o arquivo definitivo será o server.

---
+++ Q1/test.py
@@ -0,0 +1,24 @@
+# RXTX UDP
+from socket import socket,gethostbyname, AF_INET,SOCK_DGRAM
+# RXTX UDP
+SERVER_IP   = '192.168.15.11'
+PORT_NUMBER = 5000
+SIZE = 1024
+print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+
+mySocket = socket( AF_INET, SOCK_DGRAM )
+myMessage = "Hello!"
+myMessage1 = ""
+
+hostName = gethostbyname( '0.0.0.0' )
+mySocket.bind( (hostName, PORT_NUMBER) )
+
+print ("Test server listening on port {0}\n".format(PORT_NUMBER))
+
+while True:
+    mySocket.sendto(myMessage.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
+
+    (data,addr) = mySocket.recvfrom(SIZE)
+    print (data)
+
+mySocket.sendto(myMessage1.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
