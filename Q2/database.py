1dc1bca0977e8bc1dcfc13145b58a133dc22bf76
brayner1 <rbl2@cin.ufpe.br>
2017-06-20 19:24:06 -0300

    Modularização de thread.py, correção de bugs em thread.py e database.py

--- Q2/database.py
+++ Q2/database.py
@@ -1,4 +1,5 @@
 import user
+import os
 import sqlite3
 
 class DataBase(object):
@@ -14,7 +15,7 @@ class DataBase(object):
 
             CREATE TABLE IF NOT EXISTS Dirs (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                path VARCHAR(100)
+                path VARCHAR(100) UNIQUE
             );
             CREATE TABLE IF NOT EXISTS UsrDirs(
                 usrID INTEGER,
@@ -25,40 +26,65 @@ class DataBase(object):
             );""")
 
     def newFolder(self, login, folderName):
+        """ Adiciona folder no banco de dados e no sistema de arquivos em root/login/newFolder """
         self.cur.execute("""
-        INSERT INTO Dirs(path) VALUES (?)""", login + "/" + folderName)
+        INSERT INTO Dirs(path) VALUES (?)""", (login + "\\" + folderName,))
+        os.makedirs("./root/"+login+"/"+folderName)
         self.conn.commit()
 
     def newUser(self, login, password):
-        if (not self.check_user(login, password)):
+        """ Adiciona novo usuário com login e senha caso não exista usuário com nome igual.
+            Adiciona novo diretório no banco de dados"""
+        if (not self.userExists(login)):
             self.cur.execute("""
             INSERT INTO Users(login, password) VALUES (?,?)""", (login, password))
             self.cur.execute("""
             INSERT INTO Dirs(path) VALUES (?)""", (login,))
             self.conn.commit()
+            self.newFolder(login, "")
             return self.getUser(login, password)
+        return None
 
-    def newUsrDir(self, usrID, dirID):
-        self.cur.execute("""
-        INSERTO INTO UsrDirs(usrID, dirID) VALUES (?,?)""", (usrID, dirID))
-        self.conn.commit()
+    def newUsrDir(self, usr, dir):
+        """ Adiciona na tabela UsrDirs. 
+            Será usada posteriormente para o compartilhamento de arquivos e pastas."""
+        if (self.userExists(usr)):
+            self.cur.execute("""SELECT id FROM Users WHERE login=(?)""", (usr,))
+            usrID = self.cur.fetchone()
+            self.cur.execute("""SELECT id FROM Dirs Where path=(?)""", (dir,))
+            dirID = self.cur.fetchone()
+            self.cur.execute("""
+            INSERT INTO UsrDirs(usrID, dirID) VALUES (?,?)""", (usrID, dirID))
+            self.conn.commit()
 
-    def check_user(self, usuario, senha):
-        self.cur.execute("""SELECT login, password FROM Users WHERE login=(?) AND password=(?)""", (usuario, senha))
+    def check_user(self, usuario, senha) -> bool:
+        self.cur.execute("""SELECT id FROM Users WHERE login=(?) AND password=(?)""", (usuario, senha))
         result = self.cur.fetchone()
         if (result is None):
             return False
         else:
             return True
 
+    def userExists(self, usuario) -> bool:
+        self.cur.execute("""SELECT id FROM Users WHERE login=(?)""", (usuario,))
+        result = self.cur.fetchone()
+        if (result is None):
+            return False
+        else:
+            return True
     def getUser(self, usuario, senha):
         self.cur.execute("""SELECT login, password, id FROM Users WHERE login=(?) AND password=(?)""", (usuario, senha))
         result = self.cur.fetchone()
         if (result is None):
             print("Usuario ou Senha invalidos.")
-            # enviar mensagem ao usuário.
             return result
         else:
             login, passw, id = result
             usr = user.User(login, passw, id)
             return usr
+
+    def closeDB(self):
+        self.cur.close()
+        self.conn.close()
+        print("DB fechado")
+        return
--- Q2/thread.py
+++ Q2/thread.py
@@ -1,7 +1,6 @@
 import database
-import socket
-
-
+import user
+import os
 
 class Thread:
 
@@ -18,77 +17,91 @@ class Thread:
     # 3 -> getfile("path/name")
     # 4 -> pushfile("path/name", tamanho)
     # 5 -> listfiles("directory")
+    # 6 -> share(usr, "directory/file")
     def execute(self, c, data):
         lista = data.split('%')
         if (c == 1):
             # Procura pelo usuario fornecido, e se senha estiver correta, loga.
             print("Executando comando getuser")
-            login, senha = (lista[1], lista[2])
-            self.user = self.DB.getUser(login, senha)
-            msg = b''
-            if isinstance(self.user, user.User):
-                msg = b'Usuario ' + self.user.login.encode() + b' logado'
-            else:
-                msg = b'Usuario ou Senha incorretos'
-            self.conSock.send(msg)
+            self.__getUser(lista)
         elif (c == 2):
             # Cria novo usuario e loga no mesmo
             print("Executando comando createuser")
-            cmd, login, senha = data.split('%')
-            self.user = self.DB.newUser(login, senha)
-            if isinstance(self.user, user.User):
-                msg = b'Usuario ' + self.user.login.encode() + b' criado/logado'
-            else:
-                msg = b'Nome de usuario ja existente'
-            self.conSock.send(msg)
+            self.__newUser(lista)
         elif (c == 3):
             # Se estiver logado, envia arquivo para cliente
-            if self.user != None:
-                print("Executando comando getfile")
-                self.conSock.send(b'Ok')
-                cmd, fname = data.split("%")
-                try:
-                    file = open(self.user.login+"/"+fname.encode(), 'r')
-                except:
-                    msg = b'Arquivo inexistente'
-                    print(msg.decode()+".")
-                    self.conSock.send(msg)
-                    return
-                i = 0
-                while i < len(file):
-                    msg = file.read(4096)
-                    self.conSock.send(msg)
-                    i += 4096
-            else:
-                print("Nao esta logado.")
-                msg = b'Eh preciso logar'
-                self.conSock.send(msg)
+            print("Executando comando getfile")
+            self.__getFile(lista)
         elif (c == 4):
             # Se estiver logado, recebe arquivo do cliente
-            if self.user != None:
-                print("Executando comando pushfile")
-                self.conSock.send(b'Ok')
-                cmd, pathName, tam = data.split('%')
-                file = open(self.user.login+"/"+pathName, 'w')
-                while len(file) <= tam:
-                    block = self.conSock.recv(4096)
-                    file.write(block.decode())
-            else:
-                print("Nao esta logado.")
-                msg = b'Eh preciso logar'
-                self.conSock.send(msg)
+            print("Executando comando pushfile")
+            self.__pushFile(lista)
         elif (c == 5):
             # Se estiver logado, envia ao cliente a arvore de diretorios/arquivos
             if self.user != None:
-                self.conSock.send(b'Ok')
-                msg = self.__listfiles(self.user.login)
+                msg = self.__listfiles("root/"+self.user.login)
                 self.conSock.send(msg)
             else:
                 print("Nao esta logado.")
                 msg = b'Eh preciso logar'
                 self.conSock.send(msg)
 
-    def __listfiles(self, path):
+    def __getUser(self, lista):
+        login, senha = (lista[1], lista[2])
+        self.user = self.DB.getUser(login, senha)
+        msg = b''
+        if isinstance(self.user, user.User):
+            msg = b'Usuario ' + self.user.login.encode() + b' logado'
+        else:
+            msg = b'Usuario ou Senha incorretos'
+        self.conSock.send(msg)
+
+    def __newUser(self, lista):
+        login, senha = (lista[1], lista[2])
+        self.user = self.DB.newUser(login, senha)
+        if isinstance(self.user, user.User):
+            msg = b'Usuario ' + self.user.login.encode() + b' criado/logado'
+        else:
+            msg = b'Nome de usuario ja existente'
+        self.conSock.send(msg)
+
+    def __getFile(self, lista):
+        if self.user != None:
+            self.conSock.send(b'Ok')
+            fname = lista[1]
+            print("root/" + self.user.login + "/" + fname)
+            try:
+                file = open("root/"+self.user.login + "/" + fname, 'r')
+            except:
+                msg = b'Arquivo inexistente'
+                print(msg.decode() + ".")
+                self.conSock.send(msg)
+                return
+            msg = file.read().encode()
+            self.conSock.send(msg)
+        else:
+            print("Nao esta logado.")
+            msg = b'Eh preciso logar'
+            self.conSock.send(msg)
+
+    def __pushFile(self, lista):
+        if self.user != None:
+            self.conSock.send(b'Ok')
+            pathName, tam = (lista[1], lista[2])
+            os.makedirs("root/" + self.user.login + "/" + os.path.dirname(pathName), exist_ok=True)
+            file = open("root/" + self.user.login + "/" + pathName, 'w+')
+            rcvlen = 0
+            tam = int(tam)
+            while rcvlen <= tam:
+                block = self.conSock.recv(4096)
+                rcvlen += 4096
+                file.write(block.decode())
+        else:
+            print("Nao esta logado.")
+            msg = b'Eh preciso logar'
+            self.conSock.send(msg)
+
+    def __listfiles(self, path) -> bytes:
         n = path.count("/")
         scan = os.scandir(path)
         flist = [(n * "    " + x.name + "\n") for x in scan if x.is_file()]
@@ -98,11 +111,8 @@ class Thread:
         for file in flist:
             msg = msg + file.encode()
         for dir in dlist:
-            msg = msg + n*b"    "+ b"[DIR] " + dir + b":\n" + self.__listfiles(path + "/" + dir)
+            msg = msg + n*b"    " + b"[DIR] " + dir.encode() + b":\n" + self.__listfiles(path + "/" + dir)
         return msg
-
-
-
 
 
     def check_command(self, string):
@@ -130,7 +140,16 @@ class Thread:
         self.DB = database.DataBase()
         print("Thread criada para cliente:", self.conAddr)
         while True:
-            data = self.conSock.recv(1024)
+            try:
+                data = self.conSock.recv(1024)
+            except:
+                if self.user is not None:
+                    print("Closing thread for user " + self.user.login)
+                else:
+                    print("Closing thread without user")
+                self.DB.closeDB()
+                self.conSock.close()
+                return None
             if (len(data) < 1): continue
             data = data.decode()
             # Debug
